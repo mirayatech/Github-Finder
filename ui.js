48d4289143d33956f34f43598222f45082eb7d78
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-19 20:03:32 +0200

    🎨 add stying to card body

--- ui.js
+++ ui.js
@@ -9,16 +9,19 @@ class UI {
     this.profile.innerHTML = `
          <div class="card-body">
             <div class="row">
-                <div class="column"> 
+                <div class="primary-column"> 
                     <img class="profile-picture" src="${user.avatar_url}">
-                    <a href="${user.html_url}" target="_blank" class="btn">View Profile</a>
+                    <a href="${user.html_url}" target="_blank">View Profile</a>
                 </div>
-                <div class="column"> 
-                    <span class"badge-primary">Public Repos: ${user.public_repos}</span>
-                    <span class"badge-secondary">Public Gists: ${user.public_gists}</span>
-                    <span class"badge-success"> Followers: ${user.followers}</span>
-                    <span class"badge-info">Following: ${user.following}</span>
-                    <br><br>
+                <div class="wrapper-column"> 
+                    <div class="secondary-column"> 
+                        <span class"badge-primary">Public Repos: ${user.public_repos}</span>
+                        <span class"badge-secondary">Public Gists: ${user.public_gists}</span>
+                        <span class"badge-success"> Followers: ${user.followers}</span>
+                        <span class"badge-info">Following: ${user.following}</span>
+                    </div>
+
+                    
                     <ul class="list-group">
                     <li class="list-group-item">Company: ${user.company}</li>
                     <li class="list-group-item">Website/Blog: ${user.blog}</li>
@@ -26,6 +29,7 @@ class UI {
                     <li class="list-group-item">Member Since: ${user.created_at}</li>
                     </ul>
                 </div>
+                
             </div>
          </div>
          <h3 class="page-heading">Latest Repos</h3>
