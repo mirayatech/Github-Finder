827f23fd80f6af1db35d6f6444d14c2702f387aa
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-20 17:33:57 +0200

    🎨 format document

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 // Init Github
-const github = new Github;
+const github = new Github();
 
-const ui = new UI;
+const ui = new UI();
 
-const searchUser = document.getElementById('search-user');
+const searchUser = document.getElementById("search-user");
 
-searchUser.addEventListener('keyup', (e) => {
+searchUser.addEventListener("keyup", (e) => {
   const userText = e.target.value;
 
-  if(userText !== ''){
-   // Make http call
-   github.getUser(userText)
-    .then(data => {
-      if(data.profile.message === 'Not Found') {
+  if (userText !== "") {
+    // Make http call
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
       } else {
         ui.showProfile(data.profile);
         ui.showRepos(data.repos);
       }
-    })
+    });
   } else {
     // Clear profile
     ui.clearProfile();
   }
-}); 
+});
